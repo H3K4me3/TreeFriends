da40644d73d6da5cb71ddcd70cba44c51adb5793
Jialin Ma <marlin-@gmx.cn>
2019-03-29 07:59:50 -0400

    Update timetree.py

--- scripts/timetree.py
+++ scripts/timetree.py
@@ -6,13 +6,13 @@ from ete3 import Tree
 
 t = Tree()
 
-n1 = t.add_child(dist = 12.9)
+n1 = t.add_child(dist = 12.9, name = "root")
 t.add_child(name = "rheMac", dist = 28.1)
 
-n2 = n1.add_child(dist = 6.59)
+n2 = n1.add_child(dist = 6.59, name = "hg.panTro.ponAbe")
 n1.add_child(name = "ponAbe", dist = 15.2)
 
-n3 = n2.add_child(dist = 2.21)
+n3 = n2.add_child(dist = 2.21, name = "hg.panTro")
 n2.add_child(name = "gorGor", dist = 8.61)
 
 n3.add_child(name = "hg", dist = 6.4)

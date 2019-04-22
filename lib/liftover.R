0a7eef111b9700320159694d706c939c3c16f9e8
Jialin Ma <marlin-@gmx.cn>
2019-04-22 17:25:43 -0400

    Split snp_db.R

---
+++ lib/liftover.R
@@ -0,0 +1,150 @@
+
+slidingGenome <- function(width = 1000000, chr = NULL) {
+    vcffile <- VcfFile(VCF_FILE_LOC)
+    # Get chromosome and chromosome length
+    slidingRanges <- GRanges(seqinfo(BSgenome.Hsapiens.UCSC.hg38))
+    # Only keep necessary chromosomes
+    slidingRanges <- slidingRanges[seqnames(slidingRanges) %in% names(genome(vcffile))]
+    
+    slidingRanges <- slidingWindows(slidingRanges, width = width, step = width)
+    
+    # Restrict to given chromosomes
+    if (!is.null(chr))
+        slidingRanges <- slidingRanges[seqnames(slidingRanges) %in% chr]
+    
+    unname(unlist(slidingRanges, use.names = FALSE))
+}
+
+extractSNPFromVcf <- function(vcf, range = NULL) {
+    if (!is.null(range)) {
+        stopifnot(length(range) == 1)
+        # Drop unused seqlevels
+        seqlevels(range) <- as.character(unique(seqnames(range)))
+    }
+    
+    if (is.character(vcf))
+        vcf <- VariantAnnotation::VcfFile(vcf)
+    if (is(vcf, "VcfFile")) {
+        if (is.null(range))
+            vcf <- VariantAnnotation::readVcf( vcf, row.names = FALSE )
+        else
+            vcf <- VariantAnnotation::readVcf( vcf, row.names = FALSE,
+                param = ScanVcfParam(which = range)
+            )
+    }
+    stopifnot(is(vcf, "VCF"))
+    
+    #vcf <- expand(vcf)
+    gr <- rowRanges(vcf)
+    
+    ## Remove row names
+    names(gr) <- NULL
+    
+    ## Add extra columns
+    mcols(gr) <- cbind(mcols(gr), info(vcf))
+    
+    ## Remove some columns that I do not care about
+    ## - AF can be calculated from AN and AC
+    for (colname in c("paramRangeID", "QUAL", "FILTER", "AF", "VRT"))
+        mcols(gr)[[colname]] <- NULL
+    
+    ## Unlist some columns
+    for (colname in c("ALT", "AC", "Het", "Hom")) {
+        stopifnot(is(mcols(gr)[[colname]], "List"))
+        stopifnot(unique(lengths(mcols(gr)[[colname]])) == 1)
+        mcols(gr)[[colname]] <- unlist(mcols(gr)[[colname]])
+    }
+    
+    ## Fix strand
+    stopifnot(unique(strand(gr)) == "*")
+    strand(gr) <- "+"
+    
+    # Filter only SNPs
+    # FIXME: Note that when we calculate the frequency of the reference allele,
+    # we need to taking in consideration of the deletino... It's complex...
+    gr <- gr[width(gr) == 1 & width(gr$ALT) == 1]
+    
+    ## For sanity, ensure all results is within the range
+    if (!is.null(range))
+        stopifnot(countOverlaps(gr, range, type = "within") == 1)
+    ## Otherwise, we::
+    # Only select ones that has start(ans) lays inside
+    # the range to avoid duplicated results.
+    #ans <- ans[start(ans) >= start(range) & start(ans) <= end(range)]
+    
+    gr
+}
+
+snpCollapsedView <- function(snp) {
+    stopifnot(length(snp) >= 1)
+    stopifnot(is(snp, "GRanges"))
+    stopifnot(all(width(snp) == 1))
+    
+    snpdf <- tibble::as_tibble(as.data.frame(snp))
+    snpdf[["width"]] <- NULL
+    
+    grouped <- dplyr::group_by(snpdf, seqnames, start, end, strand, REF)
+    ans <- dplyr::summarize(grouped, ALL = paste0(REF, ALT, collapse = ""))
+    ans <- dplyr::ungroup(ans)
+    ans$NS <- unique(snpdf$NS)
+    ans$AN <- unique(snpdf$AN)
+    
+    # Summarize AC
+    summarize_AC <- function(char) {
+        tmp <- dplyr::select(snpdf, seqnames, start, end, strand, ALT, AC)
+        tmp <- dplyr::filter(tmp, ALT == !!char)
+        new_colname <- paste0("AC", "_", char)
+        tmp <- dplyr::rename(tmp, !!new_colname := AC)
+        tmp <- dplyr::select(tmp, -ALT)
+        tmp
+    }
+    # Join the columns
+    ## FIXME: I ignored the `Het` and `Hom` columns.
+    ans <- local({
+        nrow_before <- nrow(ans)
+        ans <- dplyr::left_join(ans, summarize_AC("A"))
+        ans <- dplyr::left_join(ans, summarize_AC("C"))
+        ans <- dplyr::left_join(ans, summarize_AC("G"))
+        ans <- dplyr::left_join(ans, summarize_AC("T"))
+        stopifnot(nrow(ans) == nrow_before)
+        ans
+    })
+    
+    ans$ALL <- mergeIUPACLetters(ans$ALL)
+    ans
+}
+
+lift_over <- function(snpdf, bsgenome, chain) {
+    stopifnot(is.data.frame(snpdf))
+    stopifnot(is(chain, "Chain"))
+    
+    gr <- as(snpdf, "GPos")
+    mcols(gr) <- NULL
+    
+    grl.target <- rtracklayer::liftOver(gr, chain)
+    gr$target.loc <- grl.target
+    
+    queryString <- function(grl, bsgenome) {
+        gr <- unlist(unname(grl))
+        dna <- DNAStringSet(Views(bsgenome, gr))
+        relist(dna, unname(grl))
+    }
+    
+    gr$target.DNA <- queryString(grl.target, bsgenome)
+    
+    # Unlist the DNAStringSetList
+    unlist_dna <- function(dsslist) {
+        len_ori <- length(dsslist)
+        dsslist[lengths(dsslist) == 0] <- DNAStringSetList(DNAStringSet("-"))
+        ans <- unlist(dsslist)
+        stopifnot(length(ans) == len_ori)
+        ans
+    }
+    gr$target.DNA <- unlist_dna(gr$target.DNA)
+    
+    ans <- mcols(gr)
+    
+    colnames(ans) <- c(paste0("loc_", providerVersion(bsgenome)),
+                       paste0("dna_", providerVersion(bsgenome)))
+    ans
+}
--- scripts/snp_db.R
+++ scripts/snp_db.R
@@ -1,4 +1,5 @@
 
+
 suppressPackageStartupMessages({
     library(here)
     
@@ -14,87 +15,16 @@ suppressPackageStartupMessages({
     library(BSgenome.PAbelii.UCSC.ponAbe2)
     library(BSgenome.Mmulatta.UCSC.rheMac8)
     #library(RSQLite)
+    library(phangorn)
 })
+
 options(verbose = TRUE)
+
+source(here("lib/liftover.R"))
+
 
 VCF_FILE_LOC = here("raw_data/ALL.TOPMed_freeze5_hg38_dbSNP.vcf.gz")
 #DB_LOC = here("results/snpdb.sqlite")
-
-slidingGenome <- function(width = 1000000, chr = NULL) {
-    vcffile <- VcfFile(VCF_FILE_LOC)
-    # Get chromosome and chromosome length
-    slidingRanges <- GRanges(seqinfo(BSgenome.Hsapiens.UCSC.hg38))
-    # Only keep necessary chromosomes
-    slidingRanges <- slidingRanges[seqnames(slidingRanges) %in% names(genome(vcffile))]
-    
-    slidingRanges <- slidingWindows(slidingRanges, width = width, step = width)
-    
-    # Restrict to given chromosomes
-    if (!is.null(chr))
-        slidingRanges <- slidingRanges[seqnames(slidingRanges) %in% chr]
-    
-    unname(unlist(slidingRanges, use.names = FALSE))
-}
-
-extractSNPFromVcf <- function(vcf, range = NULL) {
-    if (!is.null(range)) {
-        stopifnot(length(range) == 1)
-        # Drop unused seqlevels
-        seqlevels(range) <- as.character(unique(seqnames(range)))
-    }
-    
-    if (is.character(vcf))
-        vcf <- VariantAnnotation::VcfFile(vcf)
-    if (is(vcf, "VcfFile")) {
-        if (is.null(range))
-            vcf <- VariantAnnotation::readVcf( vcf, row.names = FALSE )
-        else
-            vcf <- VariantAnnotation::readVcf( vcf, row.names = FALSE,
-                param = ScanVcfParam(which = range)
-            )
-    }
-    stopifnot(is(vcf, "VCF"))
-    
-    #vcf <- expand(vcf)
-    gr <- rowRanges(vcf)
-    
-    ## Remove row names
-    names(gr) <- NULL
-    
-    ## Add extra columns
-    mcols(gr) <- cbind(mcols(gr), info(vcf))
-    
-    ## Remove some columns that I do not care about
-    ## - AF can be calculated from AN and AC
-    for (colname in c("paramRangeID", "QUAL", "FILTER", "AF", "VRT"))
-        mcols(gr)[[colname]] <- NULL
-    
-    ## Unlist some columns
-    for (colname in c("ALT", "AC", "Het", "Hom")) {
-        stopifnot(is(mcols(gr)[[colname]], "List"))
-        stopifnot(unique(lengths(mcols(gr)[[colname]])) == 1)
-        mcols(gr)[[colname]] <- unlist(mcols(gr)[[colname]])
-    }
-    
-    ## Fix strand
-    stopifnot(unique(strand(gr)) == "*")
-    strand(gr) <- "+"
-    
-    # Filter only SNPs
-    # FIXME: Note that when we calculate the frequency of the reference allele,
-    # we need to taking in consideration of the deletino... It's complex...
-    gr <- gr[width(gr) == 1 & width(gr$ALT) == 1]
-    
-    ## For sanity, ensure all results is within the range
-    if (!is.null(range))
-        stopifnot(countOverlaps(gr, range, type = "within") == 1)
-    ## Otherwise, we::
-    # Only select ones that has start(ans) lays inside
-    # the range to avoid duplicated results.
-    #ans <- ans[start(ans) >= start(range) & start(ans) <= end(range)]
-    
-    gr
-}
 
 #writeSNPToDB <- function(snp, db) {
 #    if (is.character(db)) {
@@ -118,46 +48,6 @@ extractSNPFromVcf <- function(vcf, range = NULL) {
 #    invisible(TRUE)
 #}
 
-library(dplyr)
-
-snpCollapsedView <- function(snp) {
-    stopifnot(length(snp) >= 1)
-    stopifnot(is(snp, "GRanges"))
-    stopifnot(all(width(snp) == 1))
-    
-    snpdf <- tibble::as_tibble(as.data.frame(snp))
-    snpdf[["width"]] <- NULL
-    
-    grouped <- dplyr::group_by(snpdf, seqnames, start, end, strand, REF)
-    ans <- dplyr::summarize(grouped, ALL = paste0(REF, ALT, collapse = ""))
-    ans <- dplyr::ungroup(ans)
-    ans$NS <- unique(snpdf$NS)
-    ans$AN <- unique(snpdf$AN)
-    
-    # Summarize AC
-    summarize_AC <- function(char) {
-        tmp <- dplyr::select(snpdf, seqnames, start, end, strand, ALT, AC)
-        tmp <- dplyr::filter(tmp, ALT == !!char)
-        new_colname <- paste0("AC", "_", char)
-        tmp <- dplyr::rename(tmp, !!new_colname := AC)
-        tmp <- dplyr::select(tmp, -ALT)
-        tmp
-    }
-    # Join the columns
-    ## FIXME: I ignored the `Het` and `Hom` columns.
-    ans <- local({
-        nrow_before <- nrow(ans)
-        ans <- dplyr::left_join(ans, summarize_AC("A"))
-        ans <- dplyr::left_join(ans, summarize_AC("C"))
-        ans <- dplyr::left_join(ans, summarize_AC("G"))
-        ans <- dplyr::left_join(ans, summarize_AC("T"))
-        stopifnot(nrow(ans) == nrow_before)
-        ans
-    })
-    
-    ans$ALL <- mergeIUPACLetters(ans$ALL)
-    ans
-}
 
 suppressWarnings({
     chain.panTro5 <- import.chain(here("raw_data/chainfiles/hg38ToPanTro5.over.chain"))
@@ -165,41 +55,6 @@ suppressWarnings({
     chain.ponAbe2 <- import.chain(here("raw_data/chainfiles/hg38ToPonAbe2.over.chain"))
     chain.rheMac8 <- import.chain(here("raw_data/chainfiles/hg38ToRheMac8.over.chain"))
 })
-
-lift_over <- function(snpdf, bsgenome, chain) {
-    stopifnot(is.data.frame(snpdf))
-    stopifnot(is(chain, "Chain"))
-    
-    gr <- as(snpdf, "GPos")
-    mcols(gr) <- NULL
-    
-    grl.target <- rtracklayer::liftOver(gr, chain)
-    gr$target.loc <- grl.target
-    
-    queryString <- function(grl, bsgenome) {
-        gr <- unlist(unname(grl))
-        dna <- DNAStringSet(Views(bsgenome, gr))
-        relist(dna, unname(grl))
-    }
-    
-    gr$target.DNA <- queryString(grl.target, bsgenome)
-    
-    # Unlist the DNAStringSetList
-    unlist_dna <- function(dsslist) {
-        len_ori <- length(dsslist)
-        dsslist[lengths(dsslist) == 0] <- DNAStringSetList(DNAStringSet("-"))
-        ans <- unlist(dsslist)
-        stopifnot(length(ans) == len_ori)
-        ans
-    }
-    gr$target.DNA <- unlist_dna(gr$target.DNA)
-    
-    ans <- mcols(gr)
-    
-    colnames(ans) <- c(paste0("loc_", providerVersion(bsgenome)),
-                       paste0("dna_", providerVersion(bsgenome)))
-    ans
-}
 
 lift_all <- function(snpdf) {
     d1 <- (lift_over(snpdf, BSgenome.Ptroglodytes.UCSC.panTro5, chain.panTro5))
@@ -211,10 +66,7 @@ lift_all <- function(snpdf) {
     tibble::as_tibble(as.data.frame(ans))
 }
 
-
-
 ## Phangorn staff
-library(phangorn)
 TREE = local({
     tree <- read.tree(
         text = "((((hg:6.4[&&NHX:dist=6.4:name=hg:support=1.0],panTro:6.4[&&NHX:dist=6.4:name=panTro:support=1.0])1:2.21[&&NHX:dist=2.21:name=:support=1.0],gorGor:8.61[&&NHX:dist=8.61:name=gorGor:support=1.0])1:6.59[&&NHX:dist=6.59:name=:support=1.0],ponAbe:15.2[&&NHX:dist=15.2:name=ponAbe:support=1.0])1:12.9[&&NHX:dist=12.9:name=:support=1.0],rheMac:28.1[&&NHX:dist=28.1:name=rheMac:support=1.0]);"

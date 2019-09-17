e1eefcbb2110f732b4c6c6d8216860e6ae046ecc
Jialin Ma <marlin@inventati.org>
2019-09-17 03:58:42 -0400

    Add build_rsid_position_db.py

---
+++ scripts/build_rsid_position_db.py
@@ -0,0 +1,24 @@
+
+import os
+import subprocess
+
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+PROJECT_ROOT = os.path.join(SCRIPT_DIR, "..")
+CSV_FILE = os.path.join(PROJECT_ROOT, "results", "rsid_position.csv")
+
+SH_CMD = """
+    curl http://hgdownload.cse.ucsc.edu/goldenPath/hg38/database/snp151.txt.gz | gunzip -c | awk '{if ($12 == "single") print $2 "," $4 "," $5 }'
+"""
+
+def download_csv():
+    if os.path.exists(CSV_FILE):
+        os.remove(CSV_FILE)
+    csv_file = open(CSV_FILE, "w")
+    proc = subprocess.Popen(SH_CMD, shell=True, stdout=csv_file, stderr=subprocess.PIPE)
+    proc.wait()
+    if proc.returncode > 0:
+        print("===== Download process failed with code {} =====".format(proc.returncode))
+        os.remove(CSV_FILE)
+
+if __name__ == "__main__":
+    download_csv()
